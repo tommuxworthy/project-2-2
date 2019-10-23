0670b0d63dd17eab1fdd5ddc1b7eb730cab82ef8
Tom Muxworthy <tom.muxworthy@gmail.com>
2019-10-23 10:58:04 +0100

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
+
 app.use(express.static(`${__dirname}/dist`))
 
 app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
